c78696f8056433377b950b1a0f4d3105014d2187
Pavel <pafonine@lbl.gov>
2019-01-10 11:39:04 -0800

    Fresh start

--- run_opt.py
+++ run_opt.py
@@ -1,5 +1,5 @@
 from __future__ import division
-import math, os
+import math, os, sys
 import time
 import iotbx.pdb
 from scitbx.array_family import flex
@@ -7,27 +7,41 @@ import mmtbx.model
 from libtbx.utils import null_out
 from libtbx import easy_run
 
-cmd_terachem=" ".join([
+base=[
     "qr.refine",
     "%s",
-    "restraints=qm",
-    "engine_name=xtb",
     "mode=opt",
-#    "quantum.method=\"-gfn2 -etemp 500 -gbsa h2o\"",
     "stpmax=0.2",
     "gradient_only=true", # no problem with running up to upper bound.
     "clustering=false",
     "use_convergence_test=true",
     "rmsd_tolerance=0.001",
-    "> %s.log"
-    ])
+    ]
 
-def run(cmd = cmd_terachem):
-  results_prefix = "./xtb_opt"
+cmd_cctbx    = " ".join(base+["restraints=cctbx","> %s.log"])
+cmd_xtb      = " ".join(base+["restraints=qm","engine_name=xtb",
+                              "quantum.method='-gfn2 -etemp 500 -gbsa h2o'",
+                              "> %s.log"])
+cmd_terachem = " ".join(base+["restraints=qm","engine_name=terachem",
+                              "basis=6-31g","> %s.log"])
+cmd_mopac    = " ".join(base+["restraints=qm","engine_name=mopac","> %s.log"])
+
+def run(args):
+  assert len(args)==1
+  mode = args[0]
+  assert mode in ["cctbx", "xtb", "terachem", "mopac"]
+  #
+  if(  mode=="cctbx"):    cmd = cmd_cctbx
+  elif(mode=="xtb"):      cmd = cmd_xtb
+  elif(mode=="terachem"): cmd = cmd_terachem
+  elif(mode=="mopac"):    cmd = cmd_mopac
+  else:                   assert 0
+  #
+  results_prefix = "./%s_opt"%mode
   perturbed_prefix = "./perturbed/"
   rmsd_dirs = ["0.3/","0.6/","0.9/","1.2/","1.5/"]
   base_names = ["0","1","2","3","4","5","6","7","8","9"]
-  easy_run.call("rm -rf xtb_opt")
+  easy_run.call("rm -rf %s_opt"%mode)
   os.makedirs(results_prefix)
   os.chdir(results_prefix)
   for rmsd_dir in rmsd_dirs:
@@ -45,5 +59,5 @@ def run(cmd = cmd_terachem):
 
 if __name__ == "__main__":
   t0 = time.time()
-  run()
+  run(args=sys.argv[1:])
   print "Time: %6.4f s" % (time.time() - t0) 
